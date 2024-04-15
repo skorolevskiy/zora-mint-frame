975ed21db23e49bbccdee47525110ef8b3bea705
skorolevsky <=]Zh@\rXS2KCqHG>
2024-04-15 22:03:16 +0300

    fix contract

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
-import { zora } from 'viem/chains';
+import { base } from 'viem/chains';
 import { getFrameMetadata } from '@coinbase/onchainkit';
 
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 
-export const CHAIN = zora;
-export const CONTRACT_ADDRESS = '0x774d76858551724ba815d82d90159017797560e7';
-export const TOKEN_ID = 2n; // First collection is 1
+export const CHAIN = base;
+export const CONTRACT_ADDRESS = '0x4726d6927b21409e198e5c12b2d48548d101d1f3';
+export const TOKEN_ID = 1n; // First collection is 1
 
 export const FRAME_METADATA = getFrameMetadata({
   buttons: ['Free mint'],
