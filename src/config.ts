35d0ef20048c3ab2e0b207475558558655c612a0
skorolevsky <=]Zh@\rXS2KCqHG>
2024-04-16 00:32:53 +0300

    new fix

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 
 export const CHAIN = base;
 export const CONTRACT_ADDRESS = '0x010d89d73ac069f9b828aee44eeaacdded3e7c9a';
-export const TOKEN_ID = 1n; // First collection is 1
+export const TOKEN_ID = 3n; // First collection is 1
 
 export const FRAME_METADATA = getFrameMetadata({
   buttons: ['Free mint'],
