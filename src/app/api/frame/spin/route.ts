c734dac593b71781d7d6c79e5f5953171629ca75
Sergey Korolevskiy <s.korolevskiy@gmail.com>
2024-05-04 11:31:21 +0300

    Fix spin

--- src/app/api/frame/spin/route.ts
+++ src/app/api/frame/spin/route.ts
@@ -186,7 +186,7 @@ function getResponse(type: ResponseType) {
   //   type === ResponseType.SUCCESS;
   return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${SITE_URL}/status/spin.jpg" />
+    <meta property="fc:frame:image" content="${SITE_URL}/${IMAGE}" />
     <meta property="fc:frame:image:aspect_ratio" content="1:1" />
     <meta property="fc:frame:post_url" content="${SITE_URL}/api/frame" />
 
