7292b7b957556667c3182c07becf990253bd2113
Sergey Korolevskiy <s.korolevskiy@gmail.com>
2024-05-06 23:55:57 +0300

    fix

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -56,10 +56,10 @@ export async function POST(req: NextRequest): Promise<Response> {
       const checkUser = await getUser(fid_new);
 
       if (checkUser) {
-        //await addUser(fid_new, username_new, display_name_new);
-        console.warn('added' + JSON.stringify(checkUser));
+        await addUser(fid_new, username_new, display_name_new);
+        //console.warn('added' + JSON.stringify(checkUser));
       } else {
-        console.warn('not added' + JSON.stringify(checkUser));
+        //console.warn('not added' + JSON.stringify(checkUser));
       }
 
     // // Check if user has liked and recasted
