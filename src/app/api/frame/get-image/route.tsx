ca132f429cf79a8bd249e861744b4c92fd46b71a
Sergey Korolevskiy <s.korolevskiy@gmail.com>
2024-05-18 16:34:28 +0300

    img fix

--- src/app/api/frame/get-image/route.tsx
+++ src/app/api/frame/get-image/route.tsx
@@ -3,83 +3,128 @@ import { getUser } from '../types';
 // App router includes @vercel/og.
 // No need to install it.
 
-let fid: string, name: string, points: number;
- 
+let fid: string, username: string, points: number;
+
 export async function GET(request: Request) {
-  try {
-    const { searchParams } = new URL(request.url);
- 
-    const hasFid = searchParams.has('fid');
-    const fid = hasFid ? searchParams.get('fid') : null;
+	try {
+		const { searchParams } = new URL(request.url);
 
-    const user = await getUser(fid);
+		const hasFid = searchParams.has('fid');
+		const fid = hasFid ? searchParams.get('fid') : null;
 
-    if (!user) {
+		const user = await getUser(fid);
+
+		if (!user) {
 			points = 0;
 		} else {
-      name = user.name;
+			username = user.username;
 			points = user.points;
 		}
 
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            fontSize: 40,
-            color: 'black',
-            background: '#0052FF',
-            width: '100%',
-            height: '100%',
-            padding: '50px',
-            textAlign: 'center',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            flexWrap: 'nowrap',
-          }}
-        >
-          <div
-            style={{
-              fontFamily: 'Inter, "Material Icons"',
-              fontSize: 60,
-              fontStyle: 'normal',
-              fontWeight: '700',
-              letterSpacing: '-0.025em',
-              color: 'white',
-              marginTop: 30,
-              lineHeight: 1.4,
-              whiteSpace: 'pre-wrap',
-            }}
-          >
-            Leaderboard
-          </div>
-          <div
-            style={{
-              fontFamily: 'Inter, "Material Icons"',
-              fontSize: 40,
-              fontStyle: 'normal',
-              letterSpacing: '-0.025em',
-              color: 'white',
-              marginTop: 30,
-              lineHeight: 1.4,
-              whiteSpace: 'pre-wrap',
-              display: 'flex',
-            }}
-          >
-            {name}({fid}) = {points} points
-          </div>
-        </div>
-      ),
-      {
-        width: 960,
-        height: 960,
-      },
-    );
-  } catch (e: any) {
-    console.log(`${e.message}`);
-    return new Response(`Failed to generate the image`, {
-      status: 500,
-    });
-  }
+		return new ImageResponse(
+			(
+				<div
+					style={{
+						fontSize: 40,
+						color: 'black',
+						background: '#0052FF',
+						width: '100%',
+						height: '100%',
+						padding: '50px',
+						textAlign: 'center',
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						flexDirection: 'column',
+						flexWrap: 'nowrap',
+					}}
+				>
+					<div
+						style={{
+							fontFamily: 'Inter, "Material Icons"',
+							fontSize: 60,
+							fontStyle: 'normal',
+							fontWeight: '700',
+							letterSpacing: '-0.025em',
+							color: 'white',
+							marginTop: 30,
+							lineHeight: 1.4,
+							whiteSpace: 'pre-wrap',
+						}}
+					>
+						Leaderboard
+					</div>
+					<div tw="w-full lg:w-2/3">
+						<div tw="bg-white shadow-md rounded my-6">
+							<table tw="min-w-max w-full table-auto">
+								<thead>
+									<tr tw="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+										<th tw="py-3 px-6 text-left">#</th>
+										<th tw="py-3 px-6 text-left">Nickname</th>
+										<th tw="py-3 px-6 text-center">Points</th>
+									</tr>
+								</thead>
+								<tbody tw="text-gray-600 text-sm font-light">
+									<tr tw="border-b border-gray-200 hover:bg-gray-100">
+										<td tw="py-3 px-6 text-left whitespace-nowrap">
+											<div tw="flex items-center">
+												<span tw="font-medium">1</span>
+											</div>
+										</td>
+										<td tw="py-3 px-6 text-left">
+											<div tw="flex items-center">
+												<span>{username}</span>
+											</div>
+										</td>
+										<td tw="py-3 px-6 text-center">
+											<span>{points}</span>
+										</td>
+									</tr>
+									<tr tw="border-b border-gray-200 bg-gray-50 hover:bg-gray-100">
+										<td tw="py-3 px-6 text-left whitespace-nowrap">
+											<div tw="flex items-center">
+												<span tw="font-medium">2</span>
+											</div>
+										</td>
+										<td tw="py-3 px-6 text-left">
+											<div tw="flex items-center">
+												<span>Test</span>
+											</div>
+										</td>
+										<td tw="py-3 px-6 text-center">
+											<span>1400</span>
+										</td>
+									</tr>
+									<tr tw="border-b border-gray-200 hover:bg-gray-100">
+										<td tw="py-3 px-6 text-left whitespace-nowrap">
+											<div tw="flex items-center">
+												<span tw="font-medium">3</span>
+											</div>
+										</td>
+										<td tw="py-3 px-6 text-left">
+											<div tw="flex items-center">
+												<span>Test2</span>
+											</div>
+										</td>
+										<td tw="py-3 px-6 text-center">
+											<span>1300</span>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			),
+			{
+				width: 960,
+				height: 960,
+			},
+		);
+	} catch (e: any) {
+		console.log(`${e.message}`);
+		return new Response(`Failed to generate the image`, {
+			status: 500,
+		});
+	}
 }
