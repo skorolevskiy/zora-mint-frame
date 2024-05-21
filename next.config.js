f6fdc7bb0a29821dc0d557c3338a7e6f0a1b2536
Sergey Korolevskiy <s.korolevskiy@gmail.com>
2024-05-21 23:58:13 +0300

    img fix

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-        return [
-          {
-            source: '/opengraph-image.png',
-            headers: [
-              {
-                key: 'Cache-Control',
-                value: 's-maxage=1, stale-while-revalidate=59',
-              },
-            ],
-          },
-        ];
-      },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
